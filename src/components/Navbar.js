2d663dd731301eade70ee37e6f46a51ce6975b65
Anurag <anuragtiw112@gmail.com>
2022-04-10 08:46:56 +0530

    Upto alerts(Video 13)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 
 export default function Navbar(props) {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
       <div className="container-fluid">
         <a className="navbar-brand" href="/">
           {props.title}
@@ -32,7 +32,13 @@ export default function Navbar(props) {
               </a>
             </li>
           </ul>
-          <form className="d-flex">
+          <div className={`form-check form-switch text-${props.mode==='dark'?'light':'dark'}`}>
+            <input className="form-check-input" 
+            onClick={props.toggleMode}
+            type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
+            <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable {props.mode==='dark'?'light':'dark'} Mode</label>
+        </div>
+          {/* <form className="d-flex">
             <input
               className="form-control me-2"
               type="search"
@@ -42,7 +48,7 @@ export default function Navbar(props) {
             <button className="btn btn-primary" type="submit">
               Search
             </button>
-          </form>
+          </form> */}
         </div>
       </div>
     </nav>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -7,10 +7,12 @@ export default function TextForm(props) {
     const handleUpClick = ()=>{
         let newText = text.toUpperCase();
         setText(newText);
+        props.showAlert("Converted to uppercase!", "success");
     }
     const handleLoClick = ()=>{
         let newText = text.toLowerCase();
         setText(newText);
+        props.showAlert("Converted to lowercase!", "success");
     }
     const handleOnChange = (event)=>{
         // To let us update the textarea by typing in
@@ -18,6 +20,13 @@ export default function TextForm(props) {
     }
     const handleClearClick = ()=>{
         setText('');
+        props.showAlert("Text is cleared!", "success");
+    }
+    const handleCopy = ()=>{
+        let text = document.getElementById('myBox');
+        text.select();
+        navigator.clipboard.writeText(text.value);
+        props.showAlert("Copied to clipboard", "success");
     }
     const showEmails = ()=>{
         let emailsArr = [];
@@ -34,28 +43,38 @@ export default function TextForm(props) {
         })
         setEmail(newEmails);
     }
+    const countWords = (arr)=>{
+        let res = 0;
+        arr.forEach((word)=>{
+            if(word!==''){
+                res++;
+            }
+        })
+        return res;
+    }
 
     // text = "New Text"  //Wrong way to change the state
     // setText("New Text") //Correct way to change the state
    return (
        <>
-    <div className='container'>
+    <div className='container' style={{color: props.mode==='dark'?'white': '#042743'}}>
         <h1>{props.heading}</h1>
         <div className="mb-3">
-            <textarea className='form-control' id="myBox" placeholder='Enter text here' onChange={handleOnChange} rows="7" value={text}></textarea>
+            <textarea className='form-control' style={{backgroundColor: props.mode==='dark'?'#191919': 'white',color: props.mode==='dark'?'white':'#042743'}} id="myBox" placeholder='Enter text here' onChange={handleOnChange} rows="7" value={text}></textarea>
         </div>
         <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to upperCase</button>
         <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to lowerCase</button>
         <button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear text</button>
         <button className="btn btn-primary mx-1" onClick={showEmails}>Show emails</button>
+        <button className="btn btn-primary mx-1" onClick={handleCopy}>Copy text</button>
     </div>
 
-    <div className="container my-2">
+    <div className="container my-2" style={{color: props.mode==='dark'?'white': 'black'}}>
         <h2>Your text summary</h2>
-        <p>{text.split(" ").length} words, {text.length} characters</p>
+        <p>{countWords(text.split(" "))} words, {text.length} characters</p>
         <p>{0.008*text.split(" ").length} minutes read</p>
         <h2>Preview</h2>
-        <p>{text}</p>
+        <p>{text.length>0?text:"Enter your text to get preview"}</p>
         <h2>Emails</h2>
         <p>{emails}</p>
     </div>
